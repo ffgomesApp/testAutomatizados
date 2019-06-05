cab36eae6eb6103d0cd4e1cfce1f6068cbe094b1
Fabio F Gomes <ffgomes@gmail.com>
2019-06-04 23:41:33 -0300

    testando hash de senha

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,45 @@
+const request = require("supertest");
+
+const app = require("../../src/app");
+const truncate = require("../utils/truncate");
+const { User } = require("../../src/app/models");
+
+describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it("should be able to authenticate with valid credentials", async () => {
+    const user = await User.create({
+      name: "Fabio",
+      email: "fabiogomes@gmail.com",
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not be able to authenicate with invalid credentials", async () => {
+    const user = await User.create({
+      name: "Fabio",
+      email: "fabiogomes@gmail.com",
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123456"
+      });
+
+    expect(response.status).toBe(401);
+  });
+});
